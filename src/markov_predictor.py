03e008fe6c202dadd930ea963ef50bc6beaf5356
THuynh-91 <triqhuynh91@gmail.com>
2025-11-12 09:52:10 -0500

    Implemented predict for Markov_Predictor

--- src/markov_predictor.py
+++ src/markov_predictor.py
@@ -24,10 +24,35 @@ class MarkovPredictor(RPSPredictor):
 
     # Check documentation before implementing
     def __init__(self, order: int = 3):
-        pass
+        super().__init__()
+        self.order = order
 
     def predict(self) -> str:
-        pass
+        # Not enough data - takes a random guess
+        if len(self.history) < self.order:
+            predicted_player_move = random.choice(self.MOVES)
+            return self.counter(predicted_player_move)
+        
+        # This grabs the last k moves (3) in history
+        state = tuple(self.history[-self.order:])
+
+        # Checks if we have seen this state before
+        if state not in self.transitions:
+            predicted_player_move = random.choice(self.MOVES)
+            return self.counter(predicted_player_move)
+        
+        # Finds the most common next move
+        counts = self.transitions[state]
+        total = sum(counts.values())
+
+        if total == 0:
+            predicted_player_move = random.choice(self.MOVES)
+            return self.counter(predicted_player_move)
+        
+        # Predict most likely player move
+        predicted_player_move = max(counts, key = counts.get)
+
+        return self.counter(predicted_player_move)
 
     def update(self, player_move: str, ai_move: str) -> None:
         pass
